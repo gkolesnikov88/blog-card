99ac4b05bbf8f55a92998b2a6586b38f731ab885
George Kolesnikov <gkolesnikov88@gmail.com>
2024-05-26 09:29:20 +0200

    Make card

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/card/Card";
+import cardImage from "./assets/spacejoy-YqFz7UMm8qE-unsplash.jpg";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Card 
+        <Card
+          img={cardImage}
           tags={["Interior"]}
           header="Top 5 Living Room Inspirations"
           description="Curated vibrants colors for your living, make it pop & calm in the same time."
         />
       </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import cardImage from "../../assets/spacejoy-YqFz7UMm8qE-unsplash.jpg"
+import {ReactComponent as VectorIcon} from "../../assets/Vector.svg";
 
 function Card({
   img = "",
   tags = [""],
   header = "",
-  description = "",
-  link = ""
+  description = ""
 }) {
   console.log(tags);
 
   return (
     <div className="card">
-      <div className="card-image">
-        <img src={cardImage} />
+      <div className="card-image-container">
+        <img src={img} alt="card" className="raw-image" loading="lazy"/>
       </div>
       <div className="card-content">
         <div className="tags">
@@ -26,8 +25,11 @@ function Card({
           })}
         </div>
         <h1 className="header">{header}</h1>
-        <div className="short-description">{description}</div>
-        <a className="read-more-link" href="#">Read more</a>
+        <p className="short-description">{description}</p>
+        <button className="read-more-link" disabled={false}>
+          Read more
+          <VectorIcon className="read-more-icon" aria-hidden="true"/>
+        </button> 
       </div>
     </div>
   );
